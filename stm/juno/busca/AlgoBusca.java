bcf58a320e5bcfedc10c7a06f6e34c0b2dfe2165
sergio coelho bessa da costa <sergio-coelho20111@hotmail.com>
2018-08-28 20:10:41 -0300

    implementado codigo do HyperMax

--- stm/juno/busca/AlgoBusca.java
+++ stm/juno/busca/AlgoBusca.java
@@ -6,6 +6,8 @@ import stm.juno.simulador.acoes.Jogada;
 
 public class AlgoBusca {
 
+    public static int nosExplorados;
+
     private static double[] subtrairMedia(double[] pontos) {
         double media = 0;
         for (double p : pontos)
@@ -20,6 +22,7 @@ public class AlgoBusca {
     
     public static Tuple<Jogada, double[]> NMax(Uno jogo, int profundidade) {
         profundidade++;
+        nosExplorados++;
 
         if (jogo.getIndiceGanhador() != -1 || profundidade == 15) {
             return new Tuple<>(new Jogada(), subtrairMedia(jogo.getPontuacoes()));
@@ -38,6 +41,56 @@ public class AlgoBusca {
                     alfa = psiEstrela.y[p];
                     melhorJogada = new Tuple<>(jogada, psiEstrela.y);
                 }
+            }
+
+            return melhorJogada;
+        }
+    }
+
+    private static double somar(double[] valores){
+        double soma = 0;
+        for(double p : valores)
+            soma += p;
+
+        return soma;
+    }
+
+    public static double[] getAlfa(int tamanho){
+        double[] a = new double[tamanho];
+        for (int i = 0; i < tamanho; i++){
+            a[i] = -Double.MAX_VALUE;
+        }
+        return a;
+    }
+
+    public static Tuple<Jogada, double[]> HyperMax(Uno jogo, int profundidade,double[] alfa) {
+        profundidade++;
+        nosExplorados++;
+
+        if (jogo.getIndiceGanhador() != -1 || profundidade == 15) {
+            return new Tuple<>(new Jogada(), subtrairMedia(jogo.getPontuacoes()));
+        } else {
+            Tuple<Jogada, double[]> melhorJogada = null;
+            Jogada[] jogadasPossiveis = jogo.getJogadasPossiveis();
+            for (int i = 0; i < jogadasPossiveis.length; i++) {
+                Uno clone = new Uno(jogo);
+                clone.executarJogada(jogadasPossiveis[i], false);
+                int p = jogo.getJogadores().getIndiceJogadorAtual();
+
+                double[] alfaCopia = new double[alfa.length];
+                System.arraycopy(alfa, 0, alfaCopia, 0, alfaCopia.length);
+
+                Tuple<Jogada, double[]> psiEstrela = HyperMax(clone, profundidade, alfaCopia);
+
+                if(i == 0)
+                    melhorJogada = new Tuple<>(jogadasPossiveis[i], psiEstrela.y);
+
+                if (alfa[p] < psiEstrela.y[p]) {
+                    alfa[p] = psiEstrela.y[p];
+                    melhorJogada = new Tuple<>(jogadasPossiveis[i], psiEstrela.y);
+                }
+                if(somar(alfa) >= 0)
+                    break;
             }
 
             return melhorJogada;
